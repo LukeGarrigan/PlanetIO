5148e517b12bf9da4203fbb0fc07c9874d131e11
LukeGarrigan <lukegarrigan8@gmail.com>
2017-10-20 20:57:08 +0100

    Added new bombs to the game, want to refactor the mass system, it seems a bit botch

--- SphereEating/Assets/PlayerCollision.cs
+++ SphereEating/Assets/PlayerCollision.cs
@@ -10,23 +10,27 @@ public class PlayerCollision : MonoBehaviour {
     public int minZ = -50;
     public int minX = -50;
     public GameObject food;
+    public GameObject bomb;
     public Vector3 offset;
     public float playerMass = 1;
     public Rigidbody foodMass;
+    public Rigidbody bombMass;
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "FoodTag")
         {
-           
             Destroy(collisionInfo.collider.gameObject);
             Debug.Log(foodMass.mass);
             playerMass += foodMass.mass;
-            transform.localScale += new Vector3(foodMass.mass/10, foodMass.mass/10, foodMass.mass/10);
+            transform.localScale += new Vector3(foodMass.mass / 10, foodMass.mass / 10, foodMass.mass / 10);
             Instantiate(food, new Vector3(Random.Range(minX, maxX), 0.75f, Random.Range(minZ, maxZ)), Quaternion.identity);
         }
-
-    
-
-
+        else if (collisionInfo.collider.tag == "BombTag") {
+            Destroy(collisionInfo.collider.gameObject);
+            Debug.Log(bombMass.mass);
+            //playerMass = playerMass - bombMass.mass;
+            //transform.localScale += new Vector3(bombMass.mass / 10, bombMass.mass / 10, bombMass.mass / 10);
+            Instantiate(food, new Vector3(Random.Range(minX, maxX), 0.75f, Random.Range(minZ, maxZ)), Quaternion.identity);
+        }
     }
 }
